dd510fe52b092216be1949c788344495ff073b19
novitskynick <novitskynick@gmail.com>
2023-04-21 16:19:48 +0300

    first step - rest controllers done

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,62 +3,53 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.dto.UserDto;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
-@Controller
-@RequestMapping("/admin")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin/users")
 public class AdminController {
 
     private final PasswordEncoder passwordEncoder;
     private final UserService userService;
-    private final RoleService roleService;
 
 
-    @GetMapping("/table")
-    public String usersList(ModelMap model) {
-        model.addAttribute("users", userService.getListOfUsers());
-        model.addAttribute("user", new UserDto());
-        model.addAttribute("rolesList", roleService.getListOfRolesAsListOfStrings());
-        return "admin-panel-final";
+
+    @GetMapping()
+    public List<UserDto> usersList() {
+        return userService.getListOfUsers();
     }
 
-    @PostMapping(value = "/add")
-    public String addUser(@ModelAttribute("user") UserDto userDto) {
+    @PostMapping()
+    public void addUser(@RequestBody UserDto userDto) {
         userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         userService.add(userDto);
-        return "redirect:/admin/table";
     }
 
 
-    @PostMapping(value = "/update")
-    public String updateUser(@ModelAttribute("user") UserDto userDto) {
+    @PutMapping()
+    public void updateUser(@RequestBody UserDto userDto) {
         userService.merge(userDto);
-        return "redirect:/admin/table";
     }
 
 
     @GetMapping("/user")
-    public String showLoggedInUserInfo(ModelMap model, Authentication authentication) {
-        UserDto userDto = userService.findByUsername(authentication.getName());
-        model.addAttribute("user", userDto);
-        return "user";
+    public UserDto showLoggedInUserInfo(Authentication authentication) {
+        return userService.findByUsername(authentication.getName());
     }
 
-    @GetMapping("/delete")
-    public String deleteById(@RequestParam(name = "id") Long id) {
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable(name = "id") Long id) {
         userService.deleteById(id);
-        return "redirect:/admin/table";
     }
 
     @Autowired
-    public AdminController(PasswordEncoder passwordEncoder, UserService userService, RoleService roleService) {
+    public AdminController(PasswordEncoder passwordEncoder, UserService userService) {
         this.passwordEncoder = passwordEncoder;
         this.userService = userService;
-        this.roleService = roleService;
     }
 }
