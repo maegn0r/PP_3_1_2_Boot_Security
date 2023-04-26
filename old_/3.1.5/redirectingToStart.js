d16199b29436dad2c076058f6adfa9d51e057aeb
novitskynick <novitskynick@gmail.com>
2023-04-26 14:59:38 +0300

    all works but need to clean

--- old_/3.1.5/redirectingToStart.js
+++
@@ -1,7 +0,0 @@
-function redirectingToStart() {
-    let url = 'http://localhost:8080/api/v1/admin/users'
-    fetch(url)
-        .then(response => response.json())
-        .then(data => fillingAll(data))
-        .catch(error => console.log(error))
-}
